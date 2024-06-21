cb5e8a6647d3abe1d2c0807134a214581141832e
learningmypassion <164592528+learningmypassion@users.noreply.github.com>
2024-06-21 10:42:48 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+import inquirer from "inquirer";
+let todo = [];
+let condition = true;
+while (condition) {
+    let addTask = await inquirer.prompt([
+        {
+            name: 'todo',
+            type: 'input',
+            message: 'what you want to add in your todos?'
+        },
+        {
+            name: 'addmore',
+            type: 'confirm',
+            message: 'do you want to add more?',
+            default: 'false'
+        }
+    ]);
+    todo.push(addTask.todo);
+    condition = addTask.addmore;
+    console.log(todo);
+}
